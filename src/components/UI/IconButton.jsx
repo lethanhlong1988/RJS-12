0faf32fd713c84e0fa46d8e7a24bab168f3ead5c
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-03-14 06:15:57 +0000

    update

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,10 +1,13 @@
 import { log } from "../../log.js";
 
-export default function IconButton({ children }) {
+export default function IconButton({ children, icon, ...props }) {
   log("<IconButton /> rendered", 2);
+
+  const Icon = icon;
   return (
-    <button className="button">
-      Icon <span>{children}</span>
+    <button {...props} className="button">
+      <Icon className="button-icon" />
+      <span className="button-text">{children}</span>
     </button>
   );
 }
